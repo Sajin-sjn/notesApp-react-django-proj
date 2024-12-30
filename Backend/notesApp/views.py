39ba7f2e43f3edb89385511363c344b734f72b21
Sajin PJ <sajinsjn4433@gmail.com>
2024-12-30 23:24:36 +0530

    defined notes() in views, basic request url

--- Backend/notesApp/views.py
+++ Backend/notesApp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Note
+from rest_framework.response import Response
+from .serializers import NoteSerializer
 
 # Create your views here.
+
+def notes(request):
+    notes = Note.objects.all()
+    serializer = NoteSerializer(notes, many=True)
+    return Response(serializer.data)
